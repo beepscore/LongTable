ef23cadd39ada5e768fd3e0f93cd1a4f2352107e
beepscore <support@beepscore.com>
2017-07-22 07:19:28 -0700

    Add model objet Item.swift

---
+++ LongTable/Item.swift
@@ -0,0 +1,15 @@
+//
+//  Item.swift
+//  LongTable
+//
+//  Created by Steve Baker on 7/22/17.
+//  Copyright © 2017 Beepscore LLC. All rights reserved.
+//
+
+import UIKit
+
+struct Item {
+
+    var name: String?
+    var note: String?
+}

a3398ff825adea99db53c39e3dc28b7e7874a5eb
beepscore <support@beepscore.com>
2017-08-03 01:39:01 -0700

    In DetailVC add noteLabel and use UIView.transition to page curl between views.

--- LongTable/DetailVC.swift
+++ LongTable/DetailVC.swift
@@ -12,18 +12,81 @@ class DetailVC: UIViewController {
 
     var item: Item?
 
-    @IBOutlet weak var nameLabel: UILabel!
+    var nameLabel: UILabel?
+    var noteLabel: UILabel?
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        nameLabel.text = item?.name
+        configureNameLabel()
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
 
+    func configureNameLabel() {
+        nameLabel = UILabel()
+        nameLabel?.font = UIFont.systemFont(ofSize: 24)
+        nameLabel?.textAlignment = .center
+        self.view.addSubview(nameLabel!)
+        constrainInnerView(nameLabel!, to: view)
+        nameLabel?.isUserInteractionEnabled = true
+        nameLabel?.text = item?.name
+        let nameTappedRecognizer = UITapGestureRecognizer(target: self,
+                                                          action: #selector(DetailVC.nameTapped(_:)))
+        nameLabel?.addGestureRecognizer(nameTappedRecognizer)
+    }
+
+    func configureNoteLabel() {
+        noteLabel = UILabel()
+        noteLabel?.backgroundColor = .lightGray
+        noteLabel?.font = UIFont.systemFont(ofSize: 24)
+        noteLabel?.textAlignment = .center
+        self.view.addSubview(noteLabel!)
+        constrainInnerView(noteLabel!, to: view)
+        noteLabel?.isUserInteractionEnabled = true
+        noteLabel?.text = item?.note
+        let noteTappedRecognizer = UITapGestureRecognizer(target: self,
+                                                          action: #selector(DetailVC.noteTapped(_:)))
+        noteLabel?.addGestureRecognizer(noteTappedRecognizer)
+    }
+
+    func constrainInnerView(_ innerView: UIView, to outerView: UIView) {
+        // https://stackoverflow.com/questions/26180822/swift-adding-constraints-programmatically
+        innerView.translatesAutoresizingMaskIntoConstraints = false
+        let attributes: [NSLayoutAttribute] = [.top, .bottom, .right, .left]
+        NSLayoutConstraint.activate(attributes.map {
+            NSLayoutConstraint(item: innerView, attribute: $0, relatedBy: .equal,
+                               toItem: outerView, attribute: $0, multiplier: 1, constant: 0)
+        })
+    }
+
+    func nameTapped(_ sender: UITapGestureRecognizer) {
+
+        UIView.transition(with: view,
+                          duration: 0.5,
+                          options: [UIViewAnimationOptions.transitionCurlUp],
+                          animations: {
+                            self.nameLabel?.removeFromSuperview()
+                            self.configureNoteLabel()
+        },
+                          completion: nil
+        )
+    }
+
+    func noteTapped(_ sender: UITapGestureRecognizer) {
+
+        UIView.transition(with: view,
+                          duration: 0.5,
+                          options: [UIViewAnimationOptions.transitionCurlDown],
+                          animations: {
+                            self.noteLabel?.removeFromSuperview()
+                            self.configureNameLabel()
+        },
+                          completion: nil
+        )
+    }
 }

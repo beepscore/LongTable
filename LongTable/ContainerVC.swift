9a68756acf45e38f11cbc4a2a68bf8a8dde524fb
beepscore <support@beepscore.com>
2017-08-03 14:37:41 -0700

    Add ContainerVC. Move most code from DetailVC to ContainerVC.

--- LongTable/ContainerVC.swift
+++ LongTable/ContainerVC.swift
@@ -10,15 +10,92 @@ import UIKit
 
 class ContainerVC: UIViewController {
 
+    var item: Item?
+
+    var nameLabel: UILabel?
+    var noteLabel: UILabel?
+
+
+    // MARK: - view lifecycle
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        configureNameLabel()
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - configure labels
+
+    func configureNameLabel() {
+        nameLabel = constrainedLabel(in: view)
+        nameLabel?.text = item?.name
+        let nameTappedRecognizer = UITapGestureRecognizer(target: self,
+                                                          action: #selector(ContainerVC.nameTapped(_:)))
+        nameLabel?.addGestureRecognizer(nameTappedRecognizer)
+    }
+
+    func configureNoteLabel() {
+        noteLabel = constrainedLabel(in: view)
+        noteLabel?.backgroundColor = .lightGray
+        noteLabel?.text = item?.note
+        let noteTappedRecognizer = UITapGestureRecognizer(target: self,
+                                                          action: #selector(ContainerVC.noteTapped(_:)))
+        noteLabel?.addGestureRecognizer(noteTappedRecognizer)
+    }
+
+    func constrainedLabel(in parentView: UIView) -> UILabel {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 24)
+        label.textAlignment = .center
+        parentView.addSubview(label)
+        constrainInnerView(label, to: parentView)
+        label.isUserInteractionEnabled = true
+        return label
+    }
+
+    func constrainInnerView(_ innerView: UIView, to outerView: UIView) {
+        // https://stackoverflow.com/questions/26180822/swift-adding-constraints-programmatically
+        innerView.translatesAutoresizingMaskIntoConstraints = false
+        let attributes: [NSLayoutAttribute] = [.top, .bottom, .right, .left]
+        NSLayoutConstraint.activate(attributes.map {
+            NSLayoutConstraint(item: innerView, attribute: $0, relatedBy: .equal,
+                               toItem: outerView, attribute: $0, multiplier: 1, constant: 0)
+        })
+    }
+
+    // MARK: - handle tap gestures
+
+    func nameTapped(_ sender: UITapGestureRecognizer) {
+
+        UIView.transition(with: view,
+                          duration: 0.2,
+                          // options: [UIViewAnimationOptions.transitionCurlUp],
+                          options: [UIViewAnimationOptions.transitionFlipFromTop],
+                          animations: {
+                            self.nameLabel?.removeFromSuperview()
+                            self.configureNoteLabel()
+        },
+                          completion: nil
+        )
+    }
+
+    func noteTapped(_ sender: UITapGestureRecognizer) {
+
+        UIView.transition(with: view,
+                          duration: 0.2,
+                          // options: [UIViewAnimationOptions.transitionCurlDown],
+                          options: [UIViewAnimationOptions.transitionFlipFromTop],
+                          animations: {
+                            self.noteLabel?.removeFromSuperview()
+                            self.configureNameLabel()
+        },
+                          completion: nil
+        )
     }
 
 }
--- LongTable/DetailVC.swift
+++ LongTable/DetailVC.swift
@@ -12,90 +12,32 @@ class DetailVC: UIViewController {
 
     var item: Item?
 
-    @IBOutlet weak var containerView: UIView!
-    var nameLabel: UILabel?
-    var noteLabel: UILabel?
-
-
     // MARK: - view lifecycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        configureNameLabel()
     }
-    
+
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
-    // MARK: - configure labels
+    // MARK: - Navigation
 
-    func configureNameLabel() {
-        nameLabel = constrainedLabel(in: containerView)
-        nameLabel?.text = item?.name
-        let nameTappedRecognizer = UITapGestureRecognizer(target: self,
-                                                          action: #selector(DetailVC.nameTapped(_:)))
-        nameLabel?.addGestureRecognizer(nameTappedRecognizer)
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+
+        // DetailVC has an embed segue to ContainerVC, so iOS calls prepare for segue
+        // order of execution:
+        // DetailVC.prepare(for segue: sender:)
+        // ContainerVC.viewDidLoad
+        // DetailVC.viewDidLoad
+
+        // Set containerVC.item in DetailVC prepare for segue, not in DetailVC viewDidLoad
+        // This way containerVC initial view will show text.
+
+        let containerVC = segue.destination as? ContainerVC
+        containerVC?.item = item
     }
 
-    func configureNoteLabel() {
-        noteLabel = constrainedLabel(in: containerView)
-        noteLabel?.backgroundColor = .lightGray
-        noteLabel?.text = item?.note
-        let noteTappedRecognizer = UITapGestureRecognizer(target: self,
-                                                          action: #selector(DetailVC.noteTapped(_:)))
-        noteLabel?.addGestureRecognizer(noteTappedRecognizer)
-    }
-
-    func constrainedLabel(in parentView: UIView) -> UILabel {
-        let label = UILabel()
-        label.font = UIFont.systemFont(ofSize: 24)
-        label.textAlignment = .center
-        parentView.addSubview(label)
-        constrainInnerView(label, to: parentView)
-        label.isUserInteractionEnabled = true
-        return label
-    }
-
-    func constrainInnerView(_ innerView: UIView, to outerView: UIView) {
-        // https://stackoverflow.com/questions/26180822/swift-adding-constraints-programmatically
-        innerView.translatesAutoresizingMaskIntoConstraints = false
-        let attributes: [NSLayoutAttribute] = [.top, .bottom, .right, .left]
-        NSLayoutConstraint.activate(attributes.map {
-            NSLayoutConstraint(item: innerView, attribute: $0, relatedBy: .equal,
-                               toItem: outerView, attribute: $0, multiplier: 1, constant: 0)
-        })
-    }
-
-    // MARK: - handle tap gestures
-
-    func nameTapped(_ sender: UITapGestureRecognizer) {
-
-        UIView.transition(with: containerView,
-                          duration: 0.2,
-                          // options: [UIViewAnimationOptions.transitionCurlUp],
-                          options: [UIViewAnimationOptions.transitionFlipFromTop],
-                          animations: {
-                            self.nameLabel?.removeFromSuperview()
-                            self.configureNoteLabel()
-        },
-                          completion: nil
-        )
-    }
-
-    func noteTapped(_ sender: UITapGestureRecognizer) {
-
-        UIView.transition(with: containerView,
-                          duration: 0.2,
-                          // options: [UIViewAnimationOptions.transitionCurlDown],
-                          options: [UIViewAnimationOptions.transitionFlipFromTop],
-                          animations: {
-                            self.noteLabel?.removeFromSuperview()
-                            self.configureNameLabel()
-        },
-                          completion: nil
-        )
-    }
 }

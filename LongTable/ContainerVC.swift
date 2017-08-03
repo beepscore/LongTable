2e8af5feef1034d97a8f10a7b64724e635e0a892
beepscore <support@beepscore.com>
2017-08-03 12:00:49 -0700

    Add ContainerVC

---
+++ LongTable/ContainerVC.swift
@@ -0,0 +1,24 @@
+//
+//  ContainerVC.swift
+//  LongTable
+//
+//  Created by Steve Baker on 8/3/17.
+//  Copyright © 2017 Beepscore LLC. All rights reserved.
+//
+
+import UIKit
+
+class ContainerVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
